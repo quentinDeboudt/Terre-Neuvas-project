6b890595aa8c6cf3d8ad7f8dc6cd7cda128e86b3
quentinDeboudt <Deboudtquentin@gmail.com>
2022-07-22 17:00:15 +0200

    Afficher le menu pour les Utilisateur

---
+++ src/Controller/AfficherMenuController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BoissonRepository;
+use App\Repository\DessertRepository;
+use App\Repository\EntreeRepository;
+use App\Repository\PlatRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AfficherMenuController extends AbstractController
+{
+    #[Route('/afficher/menu', name: 'app_afficher_menu')]
+    public function index(EntreeRepository $entreeRepository, PlatRepository $platRepository, DessertRepository $dessertRepository, BoissonRepository $boissonRepository): Response
+    {
+        return $this->render('afficher_menu/afficherMenu.html.twig', [
+            'LstEntrees' => $entreeRepository->findAll(),
+            'LstPLats' => $platRepository->findAll(),
+            'LstDessert' => $dessertRepository->findAll(),
+            'LstBoisson' => $boissonRepository->findAll(),
+            'controller_name' => 'MenuController',''
+        ]);
+    }
+}
