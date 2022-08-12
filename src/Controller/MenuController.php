ce2a71367f19de490b65e89d4f681cfd006d67be
quentinDeboudt <Deboudtquentin@gmail.com>
2022-08-12 15:52:31 +0200

    vérification acces au modification de la page, si connecté

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -10,9 +10,7 @@ use App\Repository\BoissonRepository;
 use App\Repository\DessertRepository;
 use App\Repository\EntreeRepository;
 use App\Repository\PlatRepository;
-use phpDocumentor\Reflection\PseudoTypes\True_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,14 +19,16 @@ class MenuController extends AbstractController
     #[Route('/menu', name: 'app_menu', methods: ['GET'])]
     public function index(EntreeRepository $entreeRepository, PlatRepository $platRepository, DessertRepository $dessertRepository, BoissonRepository $boissonRepository): Response
     {
-
-
-        return $this->render('menu/menu.html.twig', [
-            'LstEntrees' => $entreeRepository->findAll(),
-            'LstPLats' => $platRepository->findAll(),
-            'LstDesserts' => $dessertRepository->findAll(),
-            'LstBoissons' => $boissonRepository->findAll(),
-            'controller_name' => 'MenuController',''
-        ]);
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_accueil');
+        } else {
+            return $this->render('menu/menu.html.twig', [
+                'LstEntrees' => $entreeRepository->findAll(),
+                'LstPLats' => $platRepository->findAll(),
+                'LstDesserts' => $dessertRepository->findAll(),
+                'LstBoissons' => $boissonRepository->findAll(),
+                'controller_name' => 'MenuController',''
+            ]);
+        }
     }
 }
